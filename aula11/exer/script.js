f06ac3012230897c52be566c37fcb43b80f81c1f
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-20 21:45:13 -0300

    .

---
+++ aula11/exer/script.js
@@ -0,0 +1,4 @@
+let nomeAlunos3b = ['João', 'Maria', 'Neymar','Lucas', 'Sophia']
+nomeAlunos3b[4] = 'Ana julia'
+nomeAlunos3b.sort()
+console.log(nomeAlunos3b)
---
+++ aula11/index.js
@@ -0,0 +1,29 @@
+let alunos = ['joão', 'olivia', 'julia', 'sophia']
+alunos = 123
+
+console.log(typeof alunos)
+console.log(alunos instanceof Array)
+
+ 
+// console.log(alunos.slice(0,3))
+
+// delete alunos[1]
+// const removido = alunos.shift()
+// const removido = alunos.pop()
+// console.log(removido)
+
+// alunos.unshift('ronaldinho')
+// alunos.unshift('cristiano ronaldo')
+
+// alunos.push('ronaldinho')
+// alunos.push('cristiano ronaldo')
+
+// alunos[alunos.length] = 'gabriela'
+// alunos[alunos.length] = 'Ana luiza'
+
+// alunos[0] = 'duda'
+// console.log(alunos)
+
+// console.log(alunos)
+// console.log(alunos[3])
+// console.log(alunos[1])
