298360a140b913b99388b5468e6eeac2ee3f1259
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-09-24 23:02:42 -0300

    .

--- aula15/exer/script/script.js
+++ aula15/exer/script/script.js
@@ -1,1 +1,15 @@
-alert('oi')
+function meuEscopo() {
+    const form = document.querySelector('.form');
+
+    function recebeEventoForm (evento) {
+        evento.preventDefaut();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+
+        console.log(nome.value, sobrenome.value, peso.value, altura.value)
+    }
+    form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo()
