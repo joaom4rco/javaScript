4418fdc13f38c912545f62991aba688b6b79ccff
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-10 18:09:36 -0300

    .

---
+++ javaScript2/exer02/scprit03.js
@@ -0,0 +1,5 @@
+nome = 'joao', 
+sobrenome = 'santos', 
+idade = 18
+let Idade = idade *2
+console.log(`Meu nome é ${nome}, meu sobrenome é ${sobrenome} e minha idade é ${Idade} anos ` )
---
+++ javaScript2/exer02/script02.js
@@ -0,0 +1,8 @@
+let nome = '"joao"'
+console.log(nome)
+
+
+
+const conta = [1 ,'2', 5 ] 
+console.log(conta)
+console.log(typeof(conta[1]))
