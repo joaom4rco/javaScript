4125cd8d96150f69801b7a7014fbfe9890801f9e
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-11 15:20:31 -0300

    .

--- javaScript2/exer04/script.js
+++ javaScript2/exer04/script.js
@@ -1,5 +1,5 @@
 alert('escolha dois números para fazer uma conta de multiplicção')
-let num1 = prompt('digite um numero')
-let num2 = prompt('digite outro numero')
+let num1 = Number(prompt('digite um numero'))
+let num2 = Number(prompt('digite outro numero'))
 let conta = num1 * num2 
 let resultado = alert(`o resultado da sua conta foi ${conta}`)
