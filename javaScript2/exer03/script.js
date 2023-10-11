c7232a35e4c50d246e8e7a8ff80ddaf878288cb9
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-11 14:46:52 -0300

    .

---
+++ javaScript2/exer03/script.js
@@ -0,0 +1,9 @@
+const nome = 'joao mhraco'
+const sobrenome = 'santos'
+const idade = 18
+const peso = 84
+const altura = 1.8
+const imc = peso / (altura * altura)
+console.log(`Meu nome é ${nome} ${sobrenome}, eu tenho ${idade} anos`)
+console.log(`eu peso ${peso} kg e tenho ${altura} cm `)
+console.log(`e meu imc é de ${imc} `)
