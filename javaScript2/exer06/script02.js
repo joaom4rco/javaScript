298a8bd9ff5716aef23327e93f34255f126fd50d
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-11 17:41:04 -0300

    .

---
+++ javaScript2/exer06/script02.js
@@ -0,0 +1,6 @@
+let num1 ='99'
+let num2 = 55
+let num3 = 5
+num1 = Number(num1)
+const conta = num1 + num2 * num3 
+console.log(conta)
---
+++ javaScript2/exer06/script03.js
@@ -0,0 +1,4 @@
+let num1 = 0.7
+let num2 = 0.1
+conta = num1 + num2
+console.log(conta)
