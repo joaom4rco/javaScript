1c47439768ef68411142ab6bfed4197f70354fa0
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-12 18:06:07 -0300

    .

--- javaScript2/exer08/array02.js
+++ javaScript2/exer08/array02.js
@@ -1,3 +1,9 @@
 const alunos = ['joao', 'ana', 'eduada']
 alunos.push('carla')
 console.log(alunos)
+alunos.unshift('ronaldo')
+console.log(alunos)
+
+alunos.pop()
+delete alunos[1]
+console.log(alunos)
