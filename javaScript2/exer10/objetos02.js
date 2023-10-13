d8234d6fcd2253be8ea3cc442dbf06f6f2a25b8c
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-13 14:55:12 -0300

    .

--- javaScript2/exer10/objetos02.js
+++ javaScript2/exer10/objetos02.js
@@ -1,14 +1,12 @@
 function criarPessoa (nome, sobrenome, idade) {
-    return {
-        nome: nome, 
-        sobrenome: sobrenome,
+    return { nome, sobrenome: sobrenome,
         idade: idade
     };
 };
 const pessoa1 = criarPessoa('joao', 'santos', 18)
 const pessoa2 = criarPessoa('maria', 'santana', 18)
 const pessoa3 = criarPessoa('Junior', 'santana', 15)
-const pessoa4 = criarPessoa('Ronaldo', 'mmiranda', 25)
+const pessoa4 = criarPessoa('Ronaldo', 'miranda', 25)
 console.log(pessoa1)
 console.log(pessoa2)
 console.log(pessoa3)
