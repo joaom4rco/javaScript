fdf759e351640b428ef544048297a56b32756170
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-13 17:28:21 -0300

    .

--- javaScript2/exer12/js/script.js
+++ javaScript2/exer12/js/script.js
@@ -1,5 +1,8 @@
 function meuEscopo () {
     const form = document.querySelector('.form')
+    const resultado = document.querySelector('.resultado')
+
+    const pessoas = []
     // form.onsubmit = function (evento) {
     //     evento.preventDefault();
     //     alert('olá');
@@ -12,9 +15,18 @@ function meuEscopo () {
         let altura = form.querySelector('.altura');
         let peso = form.querySelector('.peso');
 
-        console.log(nome.value, sobreNome.value, altura.value, peso.value)
+        pessoas.push({
+            nome: nome.value,
+            sobreNome: sobreNome.value,
+            altura: altura.value,
+            peso: peso.value
+        });
+    
+     
+        console.log(pessoas)
+
+        resultado.innerHTML = `<p>${nome.value} ${sobreNome.value} ${altura.value} ${peso.value}  </p>`
     }
     form.addEventListener('submit', recebeEventoFormulario)
 }
-
 meuEscopo()
