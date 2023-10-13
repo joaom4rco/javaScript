a694ff55c558c8d9803cf995c790252bf1ae06c5
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-13 16:47:40 -0300

    .

--- javaScript2/exer12/js/script.js
+++ javaScript2/exer12/js/script.js
@@ -6,10 +6,13 @@ function meuEscopo () {
     //     console.log('vai se fuder');
     // };
     function recebeEventoFormulario (evento) {
-        let nome = form.querySelector('.nome');
-        let sobreNome = form.querySelector('.sobrenome');
+        evento.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobreNome = form.querySelector('.sobrenome');
         let altura = form.querySelector('.altura');
         let peso = form.querySelector('.peso');
+
+        console.log(nome.value, sobreNome.value, altura.value, peso.value)
     }
     form.addEventListener('submit', recebeEventoFormulario)
 }
