5c2a9c63a8eebc85faad0806da8244d59e31bd61
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-14 18:16:39 -0300

    .

---
+++ javaScript2/exer15/script.js
@@ -0,0 +1,12 @@
+function Campeao () {
+    const corinthians = 80
+    const Palmeiras = 70
+    const internacional = 65
+    const saoPaulo = 60
+
+    function pontuacao() {
+        const campeao = 75
+        const primeiroColocado = corinthians >= campeao 
+    }
+}
+Campeao()
