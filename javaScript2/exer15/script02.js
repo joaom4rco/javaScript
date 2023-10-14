5fc2e9e8b5d84df20392e613c71af95769e6180f
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-14 18:56:17 -0300

    .

---
+++ javaScript2/exer15/script02.js
@@ -0,0 +1,5 @@
+const hora = 18
+
+if (hora >= 0 && hora <= 12) {
+    console.log('Bom dia!')
+}
--- javaScript2/exerExtra/script.js
+++ javaScript2/exerExtra/script.js
@@ -1,12 +1,8 @@
-function Campeao () {
-    const corinthians = 80
-    const Palmeiras = 70
-    const internacional = 65
-    const saoPaulo = 60
-
-    function pontuacao() {
-        const campeao = 75
-        const primeiroColocado = corinthians >= campeao 
-    }
-}
-Campeao()
+const corinthians = 80
+const Palmeiras = 70
+const internacional = 65
+const saoPaulo = 60
+const campeao = 75
+    if (corinthians >= campeao) {
+        console.log('voce ganhou')
+    } 
