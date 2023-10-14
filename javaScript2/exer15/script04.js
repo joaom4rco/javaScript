bc1985652b2d292f065be65504201c83d590948a
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-14 20:26:36 -0300

    .

---
+++ javaScript2/exer15/script04.js
@@ -0,0 +1,11 @@
+function times () {
+    const times = ['corinthians', 'palmeiras', 'sao paulo', 'bahia' ]
+
+    function zoarTimes () {
+        const timeZuado = times[1]
+        if (timeZuado) {
+            console.log('nao tem mundial')
+        }
+    }
+}
+times()
