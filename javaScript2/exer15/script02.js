f2f3e32b9ec5824e50715e69671c0b7eada31bf0
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-14 18:58:14 -0300

    .

--- javaScript2/exer15/script02.js
+++ javaScript2/exer15/script02.js
@@ -4,4 +4,6 @@ if (hora >= 0 && hora <= 12) {
     console.log('Bom dia!')
 } else if (hora >= 13 && hora <= 17) {
     console.log('Boa tarde!')
-} 
+} else if (hora >= 18 && hora <= 23) {
+    console.log('Boa noite!')
+}
