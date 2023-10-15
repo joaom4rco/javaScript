cba265d473f161cef84bd0701f95be4ccf1193e7
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-15 19:28:29 -0300

    .

--- javaScript2/modelo/assets/js/main.js
+++ javaScript2/modelo/assets/js/main.js
@@ -1,23 +1,31 @@
-const form = document.querySelector('#form')
+const form = document.querySelector('#form');
 
-form.addEventListener('submit', function(evento){
-    evento.preventDefault();
-    console.log('evento')  
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    console.log('evento')  ;
     
-    let peso = form.querySelector('#peso')
-    let altura = form.querySelector('#altura')
+    const inputPeso = e.target.querySelector('#peso');
+    const inputAltura = e.target.querySelector('#altura');
 
-    let num1 = Number(peso)
-    let num2 = Number(altura)
-    const calcImc = num1 / (num2 * num2)
+    const peso = Number(inputPeso.value);
+    const altura = Number(inputAltura.value);
 
-    console.log(typeof(num1))
+    const calcImc = peso / (altura * altura);
 
-    mensagem(calcImc)
-})
-
+    if (calcImc < 18.5) {
+        console.log(`o seu imc é de ${calcImc.toFixed(1)} voce está abaixo do peso`)
+    }else if (calcImc >= 18.5 && calcImc <= 24.9 ) {
+        console.log(`o seu imc é de ${calcImc.toFixed(1)} voce está no peso normal`)
+    }
+});
+function 
+function criaP () {
+    const p = document.createElement(p);
+    return p ;
+};
 
 function mensagem (msg) {
-    const resultado = document.querySelector('.resultado')
-    resultado.innerHTML = msg
-}
+    const resultado = document.querySelector('.resultado');
+    resultado.innerHTML = '';
+    resultado.appendChild(p);
+};
