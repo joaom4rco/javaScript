4c506978e556f827d618cc29b6d070581c9d73ab
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-15 18:48:44 -0300

    .

--- javaScript2/modelo/assets/js/main.js
+++ javaScript2/modelo/assets/js/main.js
@@ -1,4 +1,23 @@
 const form = document.querySelector('#form')
+
 form.addEventListener('submit', function(evento){
+    evento.preventDefault();
+    console.log('evento')  
     
-});
+    let peso = form.querySelector('#peso')
+    let altura = form.querySelector('#altura')
+
+    let num1 = Number(peso)
+    let num2 = Number(altura)
+    const calcImc = num1 / (num2 * num2)
+
+    console.log(typeof(num1))
+
+    mensagem(calcImc)
+})
+
+
+function mensagem (msg) {
+    const resultado = document.querySelector('.resultado')
+    resultado.innerHTML = msg
+}
