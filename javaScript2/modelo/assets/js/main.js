e7c6f4a35f20391ca2220dfadb7d18d9057854bf
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-15 20:23:43 -0300

    .

--- javaScript2/modelo/assets/js/main.js
+++ javaScript2/modelo/assets/js/main.js
@@ -14,32 +14,34 @@ form.addEventListener('submit', function(e){
 
     if (Number.isNaN(calcImc)) {
         mensagem('nao consigo fazer a conta', false)
-        return
     }
     if (calcImc < 18.5) {
-        console.log(`o seu imc é de ${calcImc.toFixed(1)} voce está abaixo do peso`)
+        mensagem(`o seu imc é de ${calcImc.toFixed(1)} voce está abaixo do peso`)
     }else if (calcImc >= 18.5 && calcImc <= 24.9 ) {
-        console.log(`o seu imc é de ${calcImc.toFixed(1)} voce está no peso normal`)
+        mensagem(`o seu imc é de ${calcImc.toFixed(1)} voce está no peso normal`)
     }else if (calcImc >= 25 && calcImc <= 29.9) {
-        console.log(`o seu imc é de ${calcImc.toFixed(1)} voce está com Sobrepeso`)
+        mensagem(`o seu imc é de ${calcImc.toFixed(1)} voce está com Sobrepeso`)
     }else if (calcImc >= 30 && calcImc <=34.9) {
-        console.log(`o seu imc é de ${calcImc.toFixed(1)} voce está com Obesidade grau 1`)
+        mensagem(`o seu imc é de ${calcImc.toFixed(1)} voce está com Obesidade grau 1`)
     }else if (calcImc >= 35 && calcImc <= 39.9) {
-        console.log(`o seu imc é de ${calcImc.toFixed(1)} voce está com Obesidade grau 2`)
+        mensagem(`o seu imc é de ${calcImc.toFixed(1)} voce está com Obesidade grau 2`)
     }else if (calcImc >= 40 && calcImc <= 59.9) {
-        console.log(`o seu imc é de ${calcImc.toFixed(1)} voce está com Obesidade grau 3`)
-    }else if (calcImc > 60) {
-        console.log(`o seu imc é de ${calcImc.toFixed(1)} como voce está vivo??`)
+        mensagem(`o seu imc é de ${calcImc.toFixed(1)} voce está com Obesidade grau 3`)
+    }else if (calcImc >= 60) {
+        mensagem(`o seu imc é de ${calcImc.toFixed(1)} como voce está vivo??`)
     }
 });
 
 function criaP () {
-    const p = document.createElement(p);
+    const p = document.createElement('p');
     return p ;
 };
 
-function mensagem (msg) {
+function mensagem (msg, isValid) {
     const resultado = document.querySelector('.resultado');
     resultado.innerHTML = '';
+
+    const p = criaP()
+    p.innerHTML = msg
     resultado.appendChild(p);
 };
