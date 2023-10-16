adef96edeac6cef6626b1ffa44c3c3f59962ae02
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-16 16:37:16 -0300

    .

--- javaScript2/exer16/script.js
+++ javaScript2/exer16/script.js
@@ -8,8 +8,8 @@ const corPadrao = corUsuario || 'blue'
 const falaCor = `sua cor é ${corPadrao}`
 
 const nivelDeSombra = [0,1,2,3,4,5,6,7,8,9]
-const sombraUsu = nivelDeSombreamento[3]
-const sombraPadrao = sombreamentoUsu || 0
+const sombraUsu = nivelDeSombra[3]
+const sombraPadrao = sombraUsu || 0
+const falaSombra = `seu sombreamento é de ${sombraPadrao}%`
 
-const falaSombra = `seu sombreamento é de ${sombreamentoPadrao} %`
 console.log(falaCor, falaSombra)
