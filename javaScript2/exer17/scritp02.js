0d9f15a85ff2d1f452f9376a847e372cdf4171ab
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-16 17:56:49 -0300

    .

---
+++ javaScript2/exer17/scritp02.js
@@ -0,0 +1,17 @@
+function horaAEsquerda {
+    return dataBrasil 
+}
+function relogio (data) {
+    const dia = data.getDate()
+    const mes = data.getMonth() + 1
+    const ano = data.getFullYear()
+    const hora = data.getHours()
+    const min = data.getMinutes()
+    const seg = data.getSeconds()
+    const ms = data.getMilliseconds()
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+}
+const data = new Date()
+const dataBrasil = relogio(data)
+console.log(dataBrasil)
