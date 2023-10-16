15a0278950f0df2e5e2fc5b9fecae8ee2b8a7d4a
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-16 16:30:40 -0300

    .

--- javaScript2/exer16/script.js
+++ javaScript2/exer16/script.js
@@ -1,3 +1,12 @@
 const usuarioPontuacao = 500
 const nivelUsuario = usuarioPontuacao >= 1000 ? 'voce é um usuario vip' : 'voce é um usuario normal'
 console.log(nivelUsuario)
+
+
+const corUsuario = null
+const corPadrao = corUsuario || 'blue'
+const nivelDeSombreamento = [0,1,2,3,4,5,6,7,8,9]
+const sombreamentoUsu = nivelDeSombreamento[3]
+const sombreamentoPadrao = sombreamentoUsu || 0
+const fala = `seu sombreamento é`
+console.log(corPadrao, sombreamentoPadrao)
