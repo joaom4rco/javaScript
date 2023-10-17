d4dc8cfe713b22063b53568abfaf9820e4668c24
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-17 16:00:42 -0300

    .

--- javaScript2/exer18/desafio/script.js
+++ javaScript2/exer18/desafio/script.js
@@ -1,3 +1,5 @@
+const dataHojeTexto = document.getElementById('data__hoje');
+
 function getDiaSemanaTexto(diaSemana) {
     let diaSemanaTexto;
 
@@ -6,27 +8,84 @@ function getDiaSemanaTexto(diaSemana) {
             diaSemanaTexto = 'Domingo'
             return diaSemanaTexto
         case 1:
-            diaSemanaTexto = 'segunda'
+            diaSemanaTexto = 'segunda-feira'
             return diaSemanaTexto
         case 2:
-            diaSemanaTexto = 'terça'
+            diaSemanaTexto = 'terça-feira'
             return diaSemanaTexto
         case 3:
-            diaSemanaTexto = 'quarta'
+            diaSemanaTexto = 'quarta-feira'
             return diaSemanaTexto
         case 4:
-            diaSemanaTexto = 'quinta'
+            diaSemanaTexto = 'quinta-feira'
             return diaSemanaTexto
         case 5:
-            diaSemanaTexto = 'sexta'
+            diaSemanaTexto = 'sexta-feira'
             return diaSemanaTexto
         case 6:
             diaSemanaTexto = 'sabado'
             return diaSemanaTexto
     }
-}
+};
+function getMesTexto(mes) {
+    let mesTexto;
 
-const dataHoje = new Date()
-const diaSemana = dataHoje.getDay()
-let diaSemanaTexto = getDiaSemanaTexto(diaSemana)
-console.log(diaSemana, diaSemanaTexto)
+    switch (mes) {
+        case 1:
+            mesTexto = 'janeiro'
+            return mesTexto
+        case 2:
+            mesTexto = 'fevereiro'
+            return mesTexto
+        case 3:
+            mesTexto = 'março'
+            return mesTexto
+        case 4:
+            mesTexto = 'abril'
+            return mesTexto
+        case 5:
+            mesTexto = 'maio'
+            return mesTexto
+        case 6:
+            mesTexto = 'junho'
+            return mesTexto
+        case 7 :
+            mesTexto= 'julho'
+            return mesTexto
+        case 8 :
+            mesTexto= 'agosto'
+        return mesTexto    
+        case 9 :
+            mesTexto= 'setembro'
+        return mesTexto    
+        case 10 :
+            mesTexto= 'outubro'
+        return mesTexto    
+        case 11 :
+            mesTexto= 'novembro'
+        return mesTexto    
+        case 12 :
+            mesTexto= 'dezembro'
+        return mesTexto    
+    }
+};
+function anoHoraMinutos (anoHoraMin) {
+    let anoAtual = dataHoje.getFullYear()
+    let hora = dataHoje.getHours()
+    let min = dataHoje.getMinutes()
+    return `${anoAtual} as ${hora}:${min}`
+
+};
+const dataHoje = new Date();
+
+const diaSemana = dataHoje.getDay();
+let diaSemanaTexto = getDiaSemanaTexto(diaSemana);
+
+const mes = dataHoje.getMonth() + 1;
+let mesTexto = getMesTexto(mes);
+
+const anoHoraMin = anoHoraMinutos(dataHoje);
+
+dataHojeTexto.innerHTML = `<p>${diaSemanaTexto}, ${mes} de ${mesTexto} de ${anoHoraMin}`;
+
+console.log(diaSemanaTexto, mes, mesTexto, anoHoraMin);
