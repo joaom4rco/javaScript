7d403b82a081799a079e086f1e1ceafaf9ea2aa4
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-19 15:55:59 -0300

    .

--- javaScript2/exer21/desafio/script.js
+++ javaScript2/exer21/desafio/script.js
@@ -1,52 +1,17 @@
 const elementos = [
     {tag: 'p', texto: 'frase 1'},
     {tag: 'div', texto: 'frase 2'},
-    {tag: 'footer', texto: 'frase 3'},
-    {tag: 'section', texto: 'frase 4'}
+    {tag: 'div', texto: 'frase 3'},
+    {tag: 'div', texto: 'frase 4'},
+    
 ]
-
-function texto() {
-    for (let i = 0; i < elementos.length; i++) {
-        mensagem(elementos[i].texto )
-    }
+const resultado = document.querySelector('.resultado')
+const div = document.createElement('div')
+for (let i = 0; i < elementos.length; i++) {
+    let {tag, texto} = elementos[i];
+    let criaTag = document.createElement(tag);
+    let textoCriado = document.createTextNode(texto);
+    criaTag.appendChild(textoCriado);
+    div.appendChild(criaTag);
 }
-
-const texto2 = texto()
-function criarP() {
-    const p = document.createElement('p')
-    return p 
-}
-
-function criarDiv() {
-    const div = document.createElement('div')
-    return div 
-}
-
-function criarFooter() {
-    const footer = document.createElement('footer')
-    return footer 
-}
-function criarSection() {
-    const section = document.createElement('section')
-    return section 
-}
-function mensagem(msg) {
-    const resultado = document.querySelector('.resultado')
-    resultado.innerHTML = ''
-
-    const p = criarP() 
-    p.innerHTML = msg
-    resultado.appendChild(p)
-
-    const div = criarDiv() 
-    div.innerHTML = msg
-    resultado.appendChild(div)
-
-    const footer = criarFooter() 
-    footer.innerHTML = msg
-    resultado.appendChild(footer)
-
-    const section = criarSection() 
-    section.innerHTML = msg
-    resultado.appendChild(section)
-}
+resultado.appendChild(div)
