87d15de528914e36d2534ec4b4667ca2899175eb
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-20 16:38:48 -0300

    diferença entre for`s

--- javaScript2/exer22/script03.js
+++ javaScript2/exer22/script03.js
@@ -1,4 +1,11 @@
 const nomes = ['joao', 'marco', 'gabriel'] 
+
+for (let i = 0; i < nomes.length; i++) {
+    console.log(nomes[i])
+}
+
+console.log('')
+
 for (let i in nomes) {
     console.log(nomes[i])
 }
