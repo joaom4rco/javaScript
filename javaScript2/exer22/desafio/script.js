22c81f0982e359f85285f8310be16cb23c5eba7d
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-20 17:22:52 -0300

    .

---
+++ javaScript2/exer22/desafio/script.js
@@ -0,0 +1,11 @@
+const paragrafos = document.querySelector('.resultado')
+const ps = document.querySelectorAll('p')
+
+const estilosBody = getComputedStyle(document.body)
+const backgroundBody = estilosBody.backgroundColor
+console.log(backgroundBody)
+
+for (let p of ps) {
+    p.style.background = backgroundBody
+    p.style.color = 'white'
+}
