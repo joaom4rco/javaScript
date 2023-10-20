040b647fde3d3dc82ffa2e07ea72660a622136e4
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-20 16:29:01 -0300

    .

--- javaScript2/exer22/script02.js
+++ javaScript2/exer22/script02.js
@@ -1,9 +1,9 @@
 const nome = 'joao marco'
-for (let i = 0; i < nome.length; i++) {
-    console.log(i, nome[i])
+// for (let i = 0; i < nome.length; i++) {
+//     console.log(i, nome[i])
 
-};
+// };
 
 for (let i in nome) {
-    console.log
+    console.log(i)
 }
