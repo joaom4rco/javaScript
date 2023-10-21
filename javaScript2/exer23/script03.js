ad4142aae497858cdb9172696c6a4726855da47e
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-21 16:03:43 -0300

    .

---
+++ javaScript2/exer23/script03.js
@@ -0,0 +1,8 @@
+let numeros = [1,5,6,4,46,61,641,41,96,69,49,4,94,96,4196,4,964]
+
+for (let numer of numeros) {
+    if (numer === 20|| 88 || 25 ||96){
+        console.log('parando a contagem')
+        break
+    }
+}
