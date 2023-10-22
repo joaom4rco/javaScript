b7128ebaa5486608ed01636bb1578de73dec0457
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-22 14:54:37 -0300

    .

--- javaScript2/logica exer/DOM2/script/script.js
+++ javaScript2/logica exer/DOM2/script/script.js
@@ -1,10 +1,4 @@
-const idade = Number(prompt('qual é a sua idade?'))
-const resultado = document.querySelector('.resultado')
-if (idade >= 18) {
-    resultado.innerHTML = 'você é maior de idade'
-}else if (idade >= 0 & idade < 18) {
-    resultado.innerHTML = 'você é menor de idade'
-}else {
-    resultado.innerHTML = 'idade não reconhecida'
-
-}
+const botao = document.querySelector('#botao')
+botao = document.addEventListener('click', function (){
+    alert('agora seu computador está com virus ')
+})
