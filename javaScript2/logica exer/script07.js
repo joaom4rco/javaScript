e9ce1d9568e67f9c2dea5dc65654fdbbc1faa769
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-22 15:10:33 -0300

    .

---
+++ javaScript2/logica exer/script07.js
@@ -0,0 +1,12 @@
+// function string(frase) {
+//     const fra = frase
+//     return fra.slice('').map(word => word[0].toUpperCase()+ word.slice(1)).join(' ');
+// }
+// const frase = 'olá meu nome é joao'
+// const resultado = string(frase)
+// console.log(resultado)
+
+function capitalize(str) {
+    return str.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ');
+}
+console.log(capitalize("hello world"));
