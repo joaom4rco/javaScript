5be7c91f187ec0f15d08266ebef24288df0da574
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-22 13:53:08 -0300

    .

---
+++ javaScript2/logica exer/script.js
@@ -0,0 +1,8 @@
+// const pessoa = {
+//     nome: 'joao',
+//     idade: 18
+// }
+// const {nome, idade} = pessoa
+// const frase = `olá, meu nome é ${nome} e eu tenho ${idade} anos`
+// console.log(frase)
+
