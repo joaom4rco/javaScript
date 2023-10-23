0209dd4c84db34c9b325178d2a483950d170beb0
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-23 13:39:18 -0300

    .

--- javaScript2/logica exer/script09.js
+++ javaScript2/logica exer/script09.js
@@ -1,7 +1,7 @@
 function numeros (num1, num2) {
     return num1 || num2;
 }
-const num1= 51
+const num1= 80
 const num2 = 50
 const maiorNum = numeros(num1, num2)
 console.log(Math.max(maiorNum))
---
+++ javaScript2/logica exer/script10.js
@@ -0,0 +1,2 @@
+const number = (x, y) => x > y ? true : false
+console.log(number(1000, 800))
