29e22f4ecd757ae1f1f3dd0364dbac6ada06b53c
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-25 18:12:58 -0300

    adicionei uma cor para quando pausar, e fiz uma função de data

--- javaScript2/exer26/assets/js/main.js
+++ javaScript2/exer26/assets/js/main.js
@@ -2,9 +2,19 @@ const relogio = document.querySelector('.relogio')
 const iniciar = document.querySelector('.iniciar')
 const pausar = document.querySelector('.pausar')
 const zerar = document.querySelector('.zerar')
+
 iniciar.addEventListener('click', function(event) {
-    relogio.innerHTML = 
+    relogio.innerHTML = dataHora()
+})
+pausar.addEventListener('click', function(event) {
+    relogio.style.color = 'red'
 })
 zerar.addEventListener('click',function(event) {
     relogio.innerHTML = '00:00:00'
+    relogio.style.color = 'black'
 })
+function dataHora() {
+    let data = new Date()
+
+    return data.toLocaleDateString('pt-BR')
+}
