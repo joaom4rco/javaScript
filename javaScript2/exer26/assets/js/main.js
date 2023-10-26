3862098975a8bf4263b7df2906694a1b0e8e86ee
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-26 15:45:34 -0300

    .

--- javaScript2/exer26/assets/js/main.js
+++ javaScript2/exer26/assets/js/main.js
@@ -2,28 +2,17 @@ const relogio = document.querySelector('.relogio')
 const iniciar = document.querySelector('.iniciar')
 const pausar = document.querySelector('.pausar')
 const zerar = document.querySelector('.zerar')
-function data() {
-    let dataAtual = new Date()
-    dataAtual.setHours(0)
-    dataAtual.setMinutes(0)
-    dataAtual.setSeconds(0)
-    let min = dataAtual.getHours()
-    let hora = dataAtual.getMinutes()
-    let seg = dataAtual.getSeconds()
-    if (hora <10 && min >=10 && seg >= 10) {
-        return `0${hora}:${min}:${seg}`
-    }else if(min < 10 && hora >+ 10 && seg >= 10 ) {
-        return `${hora}:0${min}:${seg}`
-    }else if (seg < 10 && hora >= 10 && min >= 10) {
-        return `${hora}:${min}:0${seg}`
-    }else if (hora < 10 && min < 10 && seg < 10) {
-        return `0${hora}:0${min}:0${seg}`
-    }else {
-        return `${hora}:${min}:${seg}`
-    }
+function criarHoraDosSegundos(segundos) {
+    const data = new Date(segundos)
+    return data.toLocaleTimeString('pt-BR', {
+        hour12:false,
+        timeZone:'UTC'
+    })
 }
+let segundos = 10
+fun
 iniciar.addEventListener('click', function(event) {
-    relogio.innerHTML = data()
+    relogio.innerHTML = ''
     relogio.style.color = 'black'
 })
 pausar.addEventListener('click', function(event) {
