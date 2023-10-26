302838b05fd40bf484f0e8b4bcdbdae062c0fbe0
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-26 14:45:18 -0300

    .

--- javaScript2/exer26/assets/js/main.js
+++ javaScript2/exer26/assets/js/main.js
@@ -2,20 +2,24 @@ const relogio = document.querySelector('.relogio')
 const iniciar = document.querySelector('.iniciar')
 const pausar = document.querySelector('.pausar')
 const zerar = document.querySelector('.zerar')
-let tempo = 0
-let tempoCronometro 
-function cronometro() {
-    tempoCronometro = setInterval(function(){
-        tempo++
-        let segundos = (0 + (tempo % 60)).slice(-2);
-        let minutos = (0 + Math.floor((tempo / 60) % 60)).slice(-2);
-        let horas = (0 + Math.floor(tempo / 3600)).slice(-2);
-    })
-    return `${horas}:${minutos}:${segundos}`
+function data() {
+    let dataAtual = new Date()
+    dataAtual.setHours(0)
+    dataAtual.setMinutes(0)
+    dataAtual.setSeconds(0)
+    let min = dataAtual.getHours()
+    let hora = dataAtual.getMinutes()
+    let seg = dataAtual.getSeconds()
+    if (hora <10) {
+        return `0${hora}:${min}:${seg}`
+    }else if(min < 10) {
+        return `${hora}:0${min}:${seg}`
+    }else if (seg < 10) {
+        return `${hora}:${min}:0${seg}`
+    }else if (hora < 10 && min < 10 && seg < 10)
 }
 iniciar.addEventListener('click', function(event) {
-    
-    relogio.innerHTML = cronometro()
+    relogio.innerHTML = 
     relogio.style.color = 'black'
 })
 pausar.addEventListener('click', function(event) {
