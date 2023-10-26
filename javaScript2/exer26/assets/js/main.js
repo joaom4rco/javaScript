0b17d2c5861fd0d322e73a57a1d837d5cf5ca07d
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-26 16:01:52 -0300

    .

--- javaScript2/exer26/assets/js/main.js
+++ javaScript2/exer26/assets/js/main.js
@@ -3,23 +3,33 @@ const iniciar = document.querySelector('.iniciar')
 const pausar = document.querySelector('.pausar')
 const zerar = document.querySelector('.zerar')
 function criarHoraDosSegundos(segundos) {
-    const data = new Date(segundos)
+    const data = new Date(segundos * 1000)
     return data.toLocaleTimeString('pt-BR', {
         hour12:false,
         timeZone:'UTC'
     })
 }
-let segundos = 10
-fun
+let segundos = 0;
+let timer;
+function iniciaRelogio() {
+     timer = setInterval(function(){
+        segundos++
+        relogio.innerHTML = criarHoraDosSegundos(segundos)
+    }, 1000)
+}
 iniciar.addEventListener('click', function(event) {
-    relogio.innerHTML = ''
+    clearInterval(timer)
+    iniciaRelogio()
     relogio.style.color = 'black'
 })
 pausar.addEventListener('click', function(event) {
+    clearInterval(timer) 
     relogio.style.color = 'red'
 })
 zerar.addEventListener('click',function(event) {
+    clearInterval(timer) 
     relogio.innerHTML = '00:00:00'
+    segundos = 0
     relogio.style.color = 'black'
 })
 
