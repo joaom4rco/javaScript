4ce54c5d8d2af4fc18cdc0da2382ea00114d0e4f
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-30 13:09:01 -0300

    .

---
+++ javaScript2/logica exer/DOM3/script13.js
@@ -0,0 +1,11 @@
+
+function retangulo() {
+    const base = Number(prompt('escreva a base da seu retangulo'))
+    const altura = Number(prompt('escreva a altura da seu retangulo'))
+
+    const calcArea = base * altura
+    const calculo = Number(alert(`a área do seu rentangulo é ${calcArea}`))
+
+
+}
+retangulo()
