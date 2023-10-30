26e33af779e2c58f104435e48efd2da2162602e4
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-30 17:20:30 -0300

    .

--- javaScript2/logica exer/script17.js
+++ javaScript2/logica exer/script17.js
@@ -2,12 +2,15 @@ function mediaAno() {
     const aluno = {
         nome: 'joao',
         media1: 7,
-        media2: 4,
+        media2: 0,
         media3: 6
     }
     const {nome:nm, media1:md1, media2:md2, media3: md3} = aluno
     const calcMedia = md1 + md2 + md3 
     if (calcMedia >= 15) {
-        console.log
+        console.log(`a media do aluno ${nm} foi de ${calcMedia} e ele passou de ano`)
+    }else {
+        console.log(`a media do aluno ${nm} foi de ${calcMedia} e ele não passou de ano, e tem que fazer a recuperação`)
     }
 }
+mediaAno()
