bad3b2f2fd44c4ab16f095b3e2b20a81f37bf4b7
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-30 14:09:00 -0300

    .

--- javaScript2/logica exer/DOM4/script.js
+++ javaScript2/logica exer/DOM4/script.js
@@ -1,10 +1,10 @@
 function anoMesDia() {
     const ano = Number(prompt('escreva quantos anos você tem'))
     const mes = Number(prompt('escreva quantos meses se passou do seu ultimo aniversario'))
-    const dia = Number(prompt('escreva quantos dias se passou do seu ultimo aniversario'))
+    const dia = Number(prompt('escreva quantos dias se passou do dia do seu aniversario'))
 
     const calc = (ano * 365) + (mes * 30) + dia
-    const diaNasc = Number(prompt(`voce ja viveu ${calc} dias`))
+    const diaNasc = Number(alert(`voce ja viveu ${calc} dias`))
 
 }
 anoMesDia()
