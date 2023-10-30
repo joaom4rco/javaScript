0f0ed2a058da25a28d5affe1bb55040833f2836e
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-30 17:11:50 -0300

    .

--- javaScript2/logica exer/script16.js
+++ javaScript2/logica exer/script16.js
@@ -1,2 +1,10 @@
-const num = -5
-console.log(num >= 0? true : false)
+const num = 'tht'
+if (num > 0) {
+    console.log('positivo')
+}else if (num === 0) {
+    console.log('neutro')
+}else if (num < 0) {
+    console.log('negativo')
+}else {
+    console.log('nao reconheço esse numero')
+}
