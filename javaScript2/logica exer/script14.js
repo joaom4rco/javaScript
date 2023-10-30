4e525217e6990895644028645bf0c45c13be1e50
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-30 16:09:32 -0300

    .

--- javaScript2/logica exer/script14.js
+++ javaScript2/logica exer/script14.js
@@ -3,5 +3,10 @@ function salaMensal() {
         salario: 3000
     }
     const {salario} = fernando
-    
+    const reajusteSalarial = 100
+    const novoSalario = salario + reajusteSalarial
+    const porcentagemSalarial = (reajusteSalarial * 100/salario).toFixed(2)
+    fernando.novoSalario = novoSalario
+    console.log(porcentagemSalarial)
 }
+salaMensal()
