6b7018473e4e1065c2d100da7e19df87d99fad27
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-30 17:18:19 -0300

    ;

---
+++ javaScript2/logica exer/script17.js
@@ -0,0 +1,13 @@
+function mediaAno() {
+    const aluno = {
+        nome: 'joao',
+        media1: 7,
+        media2: 4,
+        media3: 6
+    }
+    const {nome:nm, media1:md1, media2:md2, media3: md3} = aluno
+    const calcMedia = md1 + md2 + md3 
+    if (calcMedia >= 15) {
+        console.log
+    }
+}
