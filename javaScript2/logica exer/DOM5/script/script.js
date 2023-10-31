4afc10f312bfcf3c68de9093ce72230c5a92a5a2
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-10-31 17:23:11 -0300

    .

--- javaScript2/logica exer/DOM5/script/script.js
+++ javaScript2/logica exer/DOM5/script/script.js
@@ -1,2 +1,8 @@
 const contador = document.querySelector('#botao') 
-const zerar = document.querySelector('#botao-zerar') 
+const zerar = document.querySelector('#botaozerar') 
+const resultado = document.querySelector('.resultado')
+const container = document.querySelector('.container')
+contador.addEventListener('click',(e)=>{
+    const el = e.target
+    el.classList.add('bad')
+})
