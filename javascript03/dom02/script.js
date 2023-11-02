25e0a4e3c39ee68f00cf2bb1a170a113f11a6784
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-02 15:07:40 -0300

    .

--- javascript03/dom02/script.js
+++ javascript03/dom02/script.js
@@ -1,9 +1,11 @@
 const objt = [...document.getElementsByTagName('div')]
 
-const botao = document.getElementsByTagName('button')
-botao = addEventListener('click', function(e){
-    objt.forEach(Element => {
-        Element.innerHTML = 'ola mundo'
-        objt[0].style.color = 'red'
+const botao = document.getElementById('botao-mudar')
+botao.addEventListener('click', function(e){
+         objt.forEach(Element => {
+         Element.innerHTML = 'ola mundo'
+         objt[0].style.color = 'red'
     })
+    botao.style.marginTop = '50px'
+    console.log(e)
 })
