3410ff2dc889803c8750be703fb5022fa380f99f
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-02 14:18:25 -0300

    .

--- javascript03/aula04.js
+++ javascript03/aula04.js
@@ -2,3 +2,10 @@ const jogador1 = {nome:'joao', vida:800, magia:300}
 const jogador2 = {nome:'davi', vida:800, forca:300}
 const jogador3 = {...jogador1, ...jogador2}
 console.log(jogador3)
+
+
+let soma = (v1,v2,v3,v4) => {
+    return (v1+v2+v3) / v4
+}
+let valores = [5,3,8,2]
+console.log(soma(...valores))
