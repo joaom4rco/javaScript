9337264a17ab5bd515830a6c2d4731f67f95b4c6
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-03 17:27:16 -0300

    .

--- javaScript2/logica exer/script18.js
+++ javaScript2/logica exer/script18.js
@@ -1,3 +1,5 @@
+//com 12 linhas de código
+
 // function numMaior (x,y) {
 //     if (x > y) {
 //         console.log(`${x} é maior que ${y}`)
@@ -7,7 +9,19 @@
 //         console.log(`números iguais, ${x, y}`)
 //     }
 // }
+// const x = 10
+// const y = 50
+// const num = numMaior(x,y)
+
+
+// com 4 linhas de código
+// function numMaior (x,y) {x > y ? console.log(`${x} é maior que ${y}`) : console.log(`${y} é maior que ${x}`)}
+// const x = 10
+// // const y = 50
+// const num = numMaior(x,y)
+
+
+
+// com duas linhas de código
 function numMaior (x,y) {x > y ? console.log(`${x} é maior que ${y}`) : console.log(`${y} é maior que ${x}`)}
-const x = 10
-const y = 50
-const num = numMaior(x,y)
+const num = numMaior(8,5)
