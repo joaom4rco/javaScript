2e54822428333fbbbfa97b885198fe199dc3aed0
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-03 14:49:22 -0300

    ;

--- javaScript2/logica exer/DOM6/script/script.js
+++ javaScript2/logica exer/DOM6/script/script.js
@@ -1,6 +1,20 @@
 const dataHojeTexto = document.querySelector('.container h1')
 function dia(diaSemana) {
     let diaSemanaTexto
+    switch ( diaSemana) {
+        case 0:
+            diaSemanaTexto.innerHTML = 'segunda-feira'
+            return diaSemanaTexto
+        case 0:
+            diaSemanaTexto.innerHTML = 'terça-feira'
+            return diaSemanaTexto
+            case 0:
+                diaSemanaTexto.innerHTML = 'terça-feira'
+                return diaSemanaTexto
+    }
 }
 
-const 
+const dataHj = new Date()
+
+const diaSemana = data.getDay()
+const diaSemanaTexto = dia(diaSemana)
