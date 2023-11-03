50b174a9ac3029079c94804e78173758ddb7d3ec
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-03 17:17:31 -0300

    .

---
+++ javaScript2/logica exer/script18.js
@@ -0,0 +1,12 @@
+function numMaior (x,y) {
+    if (x > y) {
+        console.log(x)
+    }else if (y > x) {
+        console.log(y)
+    } else {
+        console.log(`números iguais, ${x, y}`)
+    }
+}
+const x = 10
+const y = 50
+const num = numMaior(x,y)
