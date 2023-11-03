a457510ce7186bb715589c36e52fc011c6c988be
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-03 15:49:23 -0300

    .

--- javaScript2/logica exer/DOM6/script/script.js
+++ javaScript2/logica exer/DOM6/script/script.js
@@ -22,7 +22,7 @@ function getDiaSemanaTexto(diaSemana) {
         case 7:
             diaSemanaTexto = 'sabado'
             return diaSemanaTexto;
-    }
+    };
 }
 function getMesTexto(mes) {
     let diaMesTexto;
@@ -31,38 +31,51 @@ function getMesTexto(mes) {
             diaMesTexto = 'Janeiro'
             return diaMesTexto;
         case 2:
-            diaMesTexto = 'Janeiro'
+            diaMesTexto = 'Fevereiro'
             return diaMesTexto;
-        case 2:
-            diaMesTexto = 'Janeiro'
+        case 3:
+            diaMesTexto = 'Março'
             return diaMesTexto;
-        case 1:
-            diaMesTexto = 'Janeiro'
+        case 4:
+            diaMesTexto = 'Abril'
             return diaMesTexto;
-        case 1:
-            diaMesTexto = 'Janeiro'
+        case 5:
+            diaMesTexto = 'Maio'
             return diaMesTexto;
-        case 1:
-            diaMesTexto = 'Janeiro'
+        case 6:
+            diaMesTexto = 'Junho'
             return diaMesTexto;
-        case 1:
-            diaMesTexto = 'Janeiro'
+        case 7:
+            diaMesTexto = 'Julho'
             return diaMesTexto;
-        case 1:
-            diaMesTexto = 'Janeiro'
+        case 8:
+            diaMesTexto = 'Agosto'
             return diaMesTexto;
-        case 1:
-            diaMesTexto = 'Janeiro'
+        case 9:
+            diaMesTexto = 'Setembro'
             return diaMesTexto;
-        case 1:
-            diaMesTexto = 'Janeiro'
+        case 10:
+            diaMesTexto = 'Outubro'
             return diaMesTexto;
-        case 1:
-            diaMesTexto = 'Janeiro'
+        case 11:
+            diaMesTexto = 'Novembro'
+            return diaMesTexto;
+        case 12:
+            diaMesTexto = 'Dezembro'
             return diaMesTexto;
             
-    }
+    };
 }
+function zeroAEsquerda(min) {
+    return min >= 10 ? min : `0${min}`
+}
+function getAnoHoraMin (anoHoraMin) {
+    const anoAtual = zeroAEsquerda(dataHj.getFullYear())
+    const hora = zeroAEsquerda(dataHj.getHours())
+    const min = zeroAEsquerda(dataHj.getMinutes())
+    return `${anoAtual} ás ${hora}:${min}`
+}
+
 const dataHojeTexto = document.querySelector('.data-hoje')
 const dataHj = new Date();
 
@@ -71,8 +84,9 @@ const diaSemanaTexto = getDiaSemanaTexto(diaSemana);
 
 const diaMes = dataHj.getDate()
 
-const Mes = dataHj.getMonth() + 1 
+const mes = dataHj.getMonth() + 1 
 const diaMesTexto = getMesTexto(mes)
 
+const anoHoraMinTexto = getAnoHoraMin()
 
-dataHojeTexto.innerHTML = `${diaSemanaTexto}, ${diaMes}`
+dataHojeTexto.innerHTML = `${diaSemanaTexto}, ${diaMes} de ${diaMesTexto} em ${anoHoraMinTexto} `
