9fec550acd74ef4c55b86713f8149b8e3a865b28
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-03 14:53:19 -0300

    dias da semana criado

--- javaScript2/logica exer/DOM6/script/script.js
+++ javaScript2/logica exer/DOM6/script/script.js
@@ -1,20 +1,32 @@
-const dataHojeTexto = document.querySelector('.container h1')
+const dataHojeTexto = document.querySelector('.container h1');
 function dia(diaSemana) {
-    let diaSemanaTexto
+    let diaSemanaTexto;
     switch ( diaSemana) {
         case 0:
+            diaSemanaTexto.innerHTML = 'Domingo'
+            return diaSemanaTexto;
+        case 1:
             diaSemanaTexto.innerHTML = 'segunda-feira'
-            return diaSemanaTexto
-        case 0:
+            return diaSemanaTexto;
+        case 2:
             diaSemanaTexto.innerHTML = 'terça-feira'
-            return diaSemanaTexto
-            case 0:
-                diaSemanaTexto.innerHTML = 'terça-feira'
-                return diaSemanaTexto
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto.innerHTML = 'quarta-feira'
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto.innerHTML = 'Quinta-feira'
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto.innerHTML = 'Sexta-feira'
+            return diaSemanaTexto;
+        case 7:
+            diaSemanaTexto.innerHTML = 'sabado'
+            return diaSemanaTexto;
     }
 }
 
-const dataHj = new Date()
+const dataHj = new Date();
 
-const diaSemana = data.getDay()
-const diaSemanaTexto = dia(diaSemana)
+const diaSemana = data.getDay();
+const diaSemanaTexto = dia(diaSemana);
