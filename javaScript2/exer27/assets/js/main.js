e4cf643eb6ba73e637786c04e7b5365f16a900b7
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-05 15:55:53 -0300

    .

--- javaScript2/exer27/assets/js/main.js
+++ javaScript2/exer27/assets/js/main.js
@@ -1,3 +1,18 @@
 const inputTarefa = document.querySelector('.input-tarefa')
 const btnTarefa = document.querySelector('.btn-tarefa')
 const tarefas = document.querySelector('.tarefas')
+
+function criaLi () {
+    const li = document.createElement('li')
+    return li
+}
+function criarTarefa(textoInput) {
+    console.log(textoInput)
+    const li = criaLi()
+    li.innerText = textoInput
+    tarefas.appendChild(li)
+}
+btnTarefa.addEventListener('click', (e) =>{
+    if (!inputTarefa.value) return;
+     criarTarefa(inputTarefa.value)
+})
