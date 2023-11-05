acd2b9d64f47c6ef5f8dc79226e8f4dd878611ee
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-05 17:19:26 -0300

    .

--- javaScript2/exer27/assets/js/main.js
+++ javaScript2/exer27/assets/js/main.js
@@ -6,13 +6,41 @@ function criaLi () {
     const li = document.createElement('li')
     return li
 }
+
+inputTarefa.addEventListener('keypress', (e) => {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criarTarefa(inputTarefa.value)
+    }
+})
+
+function limpaTarefa() {
+    inputTarefa.value = ''
+    inputTarefa.focus()
+}
+function criarBotaoApagar (li) {
+    li.innerText +=' '
+    const botaoApagar = document.createElement('button')
+    botaoApagar.innerText = 'apagar'
+    botaoApagar.setAttribute('class', 'apagar')
+    botaoApagar.setAttribute('title', 'apagar esse conteúdo')
+    li.appendChild(botaoApagar)
+}
 function criarTarefa(textoInput) {
     console.log(textoInput)
     const li = criaLi()
     li.innerText = textoInput
     tarefas.appendChild(li)
+    limpaTarefa()
+    criarBotaoApagar (li)
 }
+
 btnTarefa.addEventListener('click', (e) =>{
     if (!inputTarefa.value) return;
+
      criarTarefa(inputTarefa.value)
+})
+document.addEventListener('click', (e) => {
+    const el = e.target
+    if
 })
