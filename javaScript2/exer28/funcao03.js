1d9c765287c50ce18d106e9bd409a8cd4d443c40
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-06 14:51:31 -0300

    .

--- javaScript2/exer28/funcao03.js
+++ javaScript2/exer28/funcao03.js
@@ -3,7 +3,18 @@
 // }
 // funcao(5, undefined, 20)
 
-function soma(operador, acumulador, ...numeros) {
+// function conta(operador, acumulador, ...numeros) {
+//     for (let numero of numeros) {
+//         if(operador === '+') acumulador += numero
+//         if(operador === '-') acumulador -= numero
+//         if(operador === '/') acumulador /= numero
+//         if(operador === '*') acumulador *= numero
+//     }
+//     console.log(acumulador)
+// }
+// conta('*', 2, 10,50,20,80,10)
+
+const conta = function(operador, acumulador, ...numeros) {
     for (let numero of numeros) {
         if(operador === '+') acumulador += numero
         if(operador === '-') acumulador -= numero
@@ -11,5 +22,5 @@ function soma(operador, acumulador, ...numeros) {
         if(operador === '*') acumulador *= numero
     }
     console.log(acumulador)
-}
-soma('*', 2, 10,50,20,80,10)
+};
+conta('*', 2, 10,50,20,80,10)
