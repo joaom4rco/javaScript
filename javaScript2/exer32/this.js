2ec37f6df5324f2adc92c8718b14ad0e8cb51afe
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-08 17:15:53 -0300

    .

--- javaScript2/exer32/this.js
+++ javaScript2/exer32/this.js
@@ -2,17 +2,24 @@ function criarpessoa(nome, sobrenome, altura, peso) {
     return {
         nome,
         sobrenome,
+    
+    nomeCompleto() {
+        return `${this.nome} ${this.sobrenome}`
+    },
+
 
     fala(assunto = 'esta falando oi') {
         return `${this.nome} ${assunto}`
     },
+
     altura,
     peso,
-    imc() {
+
+    get imc() {
         const contaImc = this.peso / (this.altura**this.altura)
         return contaImc.toFixed(2)
     }
     };
 };
 const p1 = criarpessoa('joao', 'marco', 1.80, 80)
-console.log(p1.imc())
+console.log(p1.imc)
