03121c220cc3e888011635c306db309307404170
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-10 15:04:32 -0300

    .

--- javaScript2/logica extra/DOM/js/script.js
+++ javaScript2/logica extra/DOM/js/script.js
@@ -3,14 +3,14 @@ function somar() {
         inputA: document.querySelector('#inputValorA'),
         inputB: document.querySelector('#inputValorB'),
         button: document.querySelector('.somar'),
-        resultado: document.querySelector('.resulltado'),
+        resultado: document.querySelector('.resultado'),
         inicia() {
             this.resultadoValores()
         },
         resultadoValores() {
             this.button.addEventListener('click', e =>{
-                const conta =  Number(this.inputA.value + this.inputB.value)
-                console.log(conta)
+                const conta =  Number(this.inputA.value)+ Number(this.inputB.value)
+                alert('o resultado da sua conta foi:' + ' ' + `${conta}`)
             })
         }
     }
