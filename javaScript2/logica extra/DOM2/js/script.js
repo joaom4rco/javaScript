3e48cbe33c24cc21562a6720cc30b52b1a60432c
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-10 15:53:17 -0300

    .

--- javaScript2/logica extra/DOM2/js/script.js
+++ javaScript2/logica extra/DOM2/js/script.js
@@ -1,11 +1,17 @@
 function somarNotaDoAluno() {
     return {
         button:document.querySelector('.somar'),
-        resultado: document.querySelector('.resultado p'),
+        resultado: document.querySelector('.resultado'),
+
         resultadoDaMedia() {
             this.button.addEventListener('click',e =>{
                 const nomeAluno = prompt('qual o nome do aluno?')
                 const primeiraNota = Number(prompt('qual foi a primeira nota?'))
+                const segundaNota = Number(prompt('qual foi a segunda nota?'))
+
+                this.resultado.innerHTML = `o nome do aluno é ${nomeAluno} <br>`
+                this.resultado.innerHTML += `a primeira nota foi ${primeiraNota} <br>`
+                this.resultado.innerHTML += `o nome do aluno é ${segundaNota}<br>`
             })
         },
 
