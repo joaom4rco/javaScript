85c49293e10cf75f12cc93b5090b5d0a54149bb6
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-10 14:26:51 -0300

    exercicio

--- javaScript2/exer33/fFabrica.js
+++ javaScript2/exer33/fFabrica.js
@@ -5,6 +5,10 @@ function Pessoa(nome, sobrenome, idade, peso, altura, imc) {
     this.peso = peso
     this.altura = altura
     this.imc = Number((peso / (altura * altura)).toFixed(2))
+
+    this.metodo = e => {
+        console.log(this.nome + ': está falando que neymar é um fracasso como jogador ')
+    }
 }
 const p1 = new Pessoa('joao', 'marco', 18, 80, 1.80)
-console.log(p1)
+p1.metodo()
