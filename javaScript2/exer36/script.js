2006ed48b32fe8ae2ca7831067f904af0822543d
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-12 13:04:12 -0300

    .

--- javaScript2/exer36/script.js
+++ javaScript2/exer36/script.js
@@ -10,13 +10,29 @@
 
 // iterando sobre a função geradora
 
-function* geradora2() {
-    yield 'valor 1'
-    yield 'valor 2'
-    yield 'valor 3'
-}
-const g2 = geradora2()
+// function* geradora2() {
+//     yield 'valor 1'
+//     yield 'valor 2'
+//     yield 'valor 3'
+// }
+// const g2 = geradora2()
 
-for (let valor of g2) {
-    console.log(valor)
-}
+// for (let valor of g2) {
+//     console.log(valor)
+// }
+
+// function* geradora3() {
+//     let i = 0
+//     while (true) {
+//         yield i
+//         i++
+//     }
+// }
+// const g3 = geradora3()
+// console.log(g3.next().value)
+// console.log(g3.next().value)
+// console.log(g3.next().value)
+// console.log(g3.next().value)
+// console.log(g3.next().value)
+// console.log(g3.next().value)
+// console.log(g3.next().value)
