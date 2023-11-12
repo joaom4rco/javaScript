f7fec3a09ae082a0104cf11b15dc2a766ac942bd
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-12 13:00:04 -0300

    iterando sobre a função geradora

--- javaScript2/exer36/script.js
+++ javaScript2/exer36/script.js
@@ -1,9 +1,22 @@
-function* geradora() {
+// function* geradora() {
+//     yield 'valor 1'
+//     yield 'valor 2'
+//     yield 'valor 3'
+// }
+// const g1 = geradora()
+// console.log(g1.next().value)
+// console.log(g1.next().value)
+// console.log(g1.next().value)
+
+// iterando sobre a função geradora
+
+function* geradora2() {
     yield 'valor 1'
     yield 'valor 2'
     yield 'valor 3'
 }
-const g1 = geradora()
-console.log(g1.next().value)
-console.log(g1.next().value)
-console.log(g1.next().value)
+const g2 = geradora2()
+
+for (let valor of g2) {
+    console.log(valor)
+}
