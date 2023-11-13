53ba4c46c02591ecf549e4e3fe5c0b2c2a2382e7
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-13 16:40:53 -0300

    .

--- javaScript2/exer37/exer/arrays.js
+++ javaScript2/exer37/exer/arrays.js
@@ -1,5 +1,17 @@
 // retorne pessoas cujo nome seja maior que 5 letras
 
+// const pessoas = [
+//     {nome:'joao', idade:18},
+//     {nome:'maria', idade:20},
+//     {nome:'wallace', idade:50},
+//     {nome:'dagoberto', idade:80},
+//     {nome:'marcos', idade:48},
+// ]
+// const pessoaComNomeGrande = pessoas.filter(obj=> obj.nome.length = 5)
+// console.log(pessoaComNomeGrande)
+
+//retorne as pessoas com mais de 50 anos
+
 const pessoas = [
     {nome:'joao', idade:18},
     {nome:'maria', idade:20},
@@ -7,5 +19,5 @@ const pessoas = [
     {nome:'dagoberto', idade:80},
     {nome:'marcos', idade:48},
 ]
-const pessoaComNomeGrande = pessoas.filter(obj=> obj.nome.length = 5)
-console.log(pessoaComNomeGrande)
+const pessoasMaisVelhas = pessoas.filter(obj => obj.idade > 50)
+console.log(pessoasMaisVelhas)
