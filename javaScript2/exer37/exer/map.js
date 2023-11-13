407449b97a027bc42fd54d2ef22049b0f70cd960
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-13 17:58:04 -0300

    .

--- javaScript2/exer37/exer/map.js
+++ javaScript2/exer37/exer/map.js
@@ -27,18 +27,32 @@
 //     { nome: 'Rosana', idade: 32 },
 //     { nome: 'Wallace', idade: 47 },
 // ];
-// const nomeDPessoa = pessoas.map(nomes => nomes.idade)
+// const nomeDPessoa = pessoas.map(nomes => {
+// delete obj.nome
+// return obj
+// })
 // console.log(nomeDPessoa)
 
 
-// adicione uma chave ID em cada elemento
+//  adicione uma chave ID em cada elemento
 const pessoas = [
-    { nome: 'Fernando', idade: 62 },
-    { nome: 'Maria', idade: 23 },
-    { nome: 'Eduarda', idade: 55 },
-    { nome: 'Letícia', idade: 60 },
-    { nome: 'Rosana', idade: 32 },
-    { nome: 'Wallace', idade: 47 },
+     { nome: 'Fernando', idade: 62 },
+     { nome: 'Maria', idade: 23 },
+     { nome: 'Eduarda', idade: 55 },
+     { nome: 'Letícia', idade: 60 },
+     { nome: 'Rosana', idade: 32 },
+     { nome: 'Wallace', idade: 47 },
 ];
-const nomeDPessoa = pessoas.map(nomes => nomes.idade)
+const nomeDPessoa = pessoas.map(obj => {
+    
+    const random = (min = 100000, max = 999999) => {
+        let ID = Math.random() * (max = min) + min
+        return Math.floor(ID)
+    };
+
+    obj.ID = random()
+    return obj
+})
 console.log(nomeDPessoa)
+
+
