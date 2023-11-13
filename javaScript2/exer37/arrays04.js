f80e5375b96aabdfeeaac313dda1371dbb95c89d
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-13 16:19:49 -0300

    melhoria

--- javaScript2/exer37/arrays04.js
+++ javaScript2/exer37/arrays04.js
@@ -1,8 +1,11 @@
-const num = [10,50,2,3,4,80,65]
+// const num = [10,50,2,3,4,80,65]
 
-const callbackFilter = (valor, indice, array) => valor > 10 ? true : false
-
+// const callbackFilter = (valor) => valor > 10 
+// const num2 = num.filter(callbackFilter)
 // num.push(90,5,50,30,15)
 
-const num2 = num.filter(callbackFilter)
+
+// outra forma de declarar
+const num = [10,50,2,3,4,80,65]
+const num2 = num.filter((valor) => valor > 10)
 console.log(num2)
