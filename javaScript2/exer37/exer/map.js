fd91cff9757bb7af4fd12c15abd57e7c6569c696
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-13 17:31:20 -0300

    usando o método map

---
+++ javaScript2/exer37/exer/map.js
@@ -0,0 +1,19 @@
+// const numeros = [11,5,1,6,8,4,2,2,0]
+// const numerosDobrados = numeros.map(num => num * 2)
+// console.log(numerosDobrados)
+
+//para cada elemento:
+//retorne só a string com o nome da pessoa
+
+const pessoas = [
+    { nome: 'Fernando', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduarda', idade: 55 },
+    { nome: 'Letícia', idade: 60 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+];
+const nomeDPessoa = pessoas.map(nomes => nomes.nome)
+console.log(nomeDPessoa)
+
+
