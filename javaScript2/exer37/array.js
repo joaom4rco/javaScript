30212a5884af29dd66206517e9310f4effb97ce0
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-13 15:03:08 -0300

    .

--- javaScript2/exer37/array.js
+++ javaScript2/exer37/array.js
@@ -1,5 +1,16 @@
 //mudando o valor do array e deletando ele
+// const nomes = ['eduardo','maria','antonio']
+// nomes[2] = 'joao'
+// delete nomes[2]
+// console.log(nomes)
+
+
+// valor por referencia
+// const nomes = ['eduardo','maria','antonio']
+// const novo = nomes.pop()
+// console.log(nomes, novo)
+
+// copia
 const nomes = ['eduardo','maria','antonio']
-nomes[2] = 'joao'
-delete nomes[2]
-console.log(nomes)
+const novo = nomes.pop()
+console.log(nomes, novo)
