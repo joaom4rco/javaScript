4f475ef5f89662dc41aa97ff9885f6d0e094b81d
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-13 17:22:50 -0300

    .

--- javaScript2/exer37/exer/arrays.js
+++ javaScript2/exer37/exer/arrays.js
@@ -24,12 +24,25 @@
 
 // retorne o nome de pessoas que termina com a 
 
+// const pessoas = [
+//     {nome:'joao', idade:18},
+//     {nome:'maria', idade:20},
+//     {nome:'wallace', idade:50},
+//     {nome:'dagoberto', idade:80},
+//     {nome:'marcos', idade:48},
+// ]
+// const nomeQTerminaComA = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('a'))
+// console.log(nomeQTerminaComA)
+
 const pessoas = [
-    {nome:'joao', idade:18},
-    {nome:'maria', idade:20},
-    {nome:'wallace', idade:50},
-    {nome:'dagoberto', idade:80},
-    {nome:'marcos', idade:48},
-]
-const nomeQTerminaComA = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('a'))
-console.log(nomeQTerminaComA)
+    { nome: 'Fernando', idade: 62 },
+    { nome: 'Maria', idade: 23 },
+    { nome: 'Eduarda', idade: 55 },
+    { nome: 'Letícia', idade: 60 },
+    { nome: 'Rosana', idade: 32 },
+    { nome: 'Wallace', idade: 47 },
+];
+   
+const pessoas2 = pessoas.filter(n => n.nome.length >= 5).filter(i => i.idade > 50).filter(l => l.nome.toLowerCase().endsWith('a'));
+console.log(pessoas2);
+   
