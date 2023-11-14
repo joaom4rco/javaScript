327a8e9099369708c108f7d7fd159559013cdd2d
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-14 15:33:36 -0300

    .

--- javaScript2/exer37/exer02/unindoTUDO.js
+++ javaScript2/exer37/exer02/unindoTUDO.js
@@ -6,6 +6,7 @@
 const numeros = [4,5,68,55,22,3];
 
 const resultado = numeros
+
     .filter(valor => valor % 2 ===0)
     .map(valor => valor * 2 )
     .reduce((ac, valor) =>  ac + valor);
---
+++ javaScript2/logica exer/DOM12/script/script.js
@@ -0,0 +1,11 @@
+const fontButton1 = document.querySelector('#size-button1')
+const fontButton2 = document.querySelector('#size-button2')
+const fontButton3 = document.querySelector('#size-button3')
+
+const button = document.querySelector('#button')
+
+button.addEventListener('click',() =>  {
+    fontButton1.style.fontSize = 30 +'px'
+    fontButton2.style.color = 'red'
+    fontButton3.style.backgroundColor = 'black'
+})
