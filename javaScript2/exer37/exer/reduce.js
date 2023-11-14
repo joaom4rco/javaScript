eb92091c14414e2bd90846522995b80e64d51d0b
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-14 14:51:19 -0300

    .

--- javaScript2/exer37/exer/reduce.js
+++ javaScript2/exer37/exer/reduce.js
@@ -1,8 +1,16 @@
 // some todos os números de um array
-//
+
+// const numeros = [1,2,3,4,5]
+// const total = numeros.reduce((acumulador, valor) =>{
+//     return acumulador + valor
+// })
+// console.log(total)
+
+// retorne um array com os pares
 
 const numeros = [1,2,3,4,5]
 const total = numeros.reduce((acumulador, valor) =>{
-    return acumulador + valor
-})
+    if (valor % 2 ===0) acumulador.push(valor)
+    return acumulador
+}, [])
 console.log(total)
