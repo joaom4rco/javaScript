bcc0e06104c4de4a05639c93f9035258939e2952
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-14 15:08:35 -0300

    .

--- javaScript2/exer37/exer/reduce.js
+++ javaScript2/exer37/exer/reduce.js
@@ -8,9 +8,26 @@
 
 // retorne um array com os pares
 
-const numeros = [1,2,3,4,5]
-const total = numeros.reduce((acumulador, valor) =>{
-    if (valor % 2 ===0) acumulador.push(valor)
-    return acumulador
-}, [])
-console.log(total)
+// const numeros = [1,2,3,4,5]
+// const total = numeros.reduce((acumulador, valor) =>{
+//     if (valor % 2 ===0) acumulador.push(valor)
+//     return acumulador
+// }, [])
+// console.log(total)
+
+//  retorne a pessoa mais velha
+
+const pessoas = [
+    { nome: 'Fernando', idade: 62 },
+    { nome: 'Maria', idade: 63 },
+    { nome: 'Eduarda', idade: 85 },
+    { nome: 'Letícia', idade: 10 },
+    { nome: 'Rosana', idade: 82 },
+    { nome: 'Wallace', idade: 17 },
+];
+
+const pessoaMaisVelha = pessoas.reduce((acumulador, valor) => {
+    if(acumulador.idade > valor.idade) return acumulador
+    return valor
+})
+console.log(pessoaMaisVelha)
