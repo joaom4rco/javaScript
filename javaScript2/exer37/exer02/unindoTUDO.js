14a2dbec3142ca08b3887e833d80cb88394643d7
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-14 15:19:24 -0300

    .

--- javaScript2/exer37/exer02/unindoTUDO.js
+++ javaScript2/exer37/exer02/unindoTUDO.js
@@ -3,12 +3,11 @@
 // =>dobrar os valores
 // => reduzir(somar tudo)
 
-const numeros = [4,5,68,55,22,3]
-const resultado = numeros.filter(valor => {
-    return valor % 2 ===0
-}).map(valor =>{
-    return valor * 2 
-}).reduce((ac, valor) => {
-    return ac + valor
-})
-console.log(resultado)
+const numeros = [4,5,68,55,22,3];
+
+const resultado = numeros
+    .filter(valor => valor % 2 ===0)
+    .map(valor => valor * 2 )
+    .reduce((ac, valor) =>  ac + valor);
+
+console.log(resultado);
