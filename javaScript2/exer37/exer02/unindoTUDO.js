2b2a074dd8a7345b4a4afbe71f154219a035a6e3
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-14 15:18:16 -0300

    .

---
+++ javaScript2/exer37/exer02/unindoTUDO.js
@@ -0,0 +1,14 @@
+// retorne a soma do dobro de todos os pares
+// => filtrar pares
+// =>dobrar os valores
+// => reduzir(somar tudo)
+
+const numeros = [4,5,68,55,22,3]
+const resultado = numeros.filter(valor => {
+    return valor % 2 ===0
+}).map(valor =>{
+    return valor * 2 
+}).reduce((ac, valor) => {
+    return ac + valor
+})
+console.log(resultado)
