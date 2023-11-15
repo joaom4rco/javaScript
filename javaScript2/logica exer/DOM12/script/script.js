f7dfa40231fbd40409ff7227f73d243c6b070785
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-15 15:28:16 -0300

    .

--- javaScript2/logica exer/DOM12/script/script.js
+++ javaScript2/logica exer/DOM12/script/script.js
@@ -2,7 +2,7 @@ function inputNum() {
     return {
         fnum: document.querySelector('#fnum'),
         contar:document.querySelector('#fcontar'),
-        resultado: document.querySelector('#resultado'),
+        resultado: document.querySelector('#resultado p'),
 
         valor() {
             this.botao()
@@ -11,15 +11,19 @@ function inputNum() {
         botao() {
             this.contar.addEventListener('click', e =>{
                 this.forInput()
+                this.fnum.value = ''
             })
         },
 
         forInput() {
             const valorInput = this.fnum.value
             for(let i = 0; i <= valorInput; i++ ) {
-                this.resultado.innerHTML += i
+                this.resultado.innerHTML += ` ${i} &#x1F449`
             }
-        }
+            this.resultado.innerHTML += '&#x1F3C1'
+        },
+
+        
     }
 }
 const input = inputNum()
