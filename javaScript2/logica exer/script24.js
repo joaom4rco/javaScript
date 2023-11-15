79f116cb34479b06c3467a223642e7dba1d508f6
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-15 16:56:14 -0300

    .

--- javaScript2/logica exer/script24.js
+++ javaScript2/logica exer/script24.js
@@ -1,4 +1,31 @@
-function numeroContaCliente() {
-    const contaCliente = Math.random() * (999 - 100) + 999
-    return contaCliente 
+function Cliente() {
+    return {
+        infoCliente() {
+            this.numeroConta()
+            this.conta()
+        },
+        numeroConta() {
+        const contaCliente = Math.random() * (999 - 100) + 100
+        console.log(Math.floor(contaCliente)) 
+        },
+        conta() {
+            const conta = this.saldoCliente() + this.creditoCliente() - this.debitoCliente()
+            console.log(conta)
+        },
+        saldoCliente() {
+            const saldo = Math.random() * (999-0 )+100
+            return Math.floor(saldo) 
+        },
+        creditoCliente() {
+            const credito = Math.random() * (200-0) +50
+            return Math.floor(credito)
+        },
+        debitoCliente() {
+            const credito = Math.random() * (999-0) +80
+            return  Math.floor(credito)
+        },
+    }
+      
 }
+const novoCliente = Cliente()
+novoCliente.infoCliente()
