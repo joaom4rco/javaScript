99ff141b272555b70e80c8291e3f10197e74f3aa
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-16 16:40:20 -0300

    .

--- javaScript2/logica exer/DOM14/script/script.js
+++ javaScript2/logica exer/DOM14/script/script.js
@@ -2,9 +2,19 @@ function validaSenha() {
     return {
         inputSenha: document.querySelector('#isenha'),
         botaoSenha: document.querySelector('#button-Senha'),
-        
         senha() {
+            this.botaoSenha.addEventListener('click', e => {
+                const senhaUsuario = this.inputSenha.value
 
+                const arraySenha = []
+                arraySenha.push(senhaUsuario)
+                
+                console.log(arraySenha);
+
+                this.inputSenha.value = ''
+            }) 
         }
     }
 }
+const senha = validaSenha()
+senha.senha()
