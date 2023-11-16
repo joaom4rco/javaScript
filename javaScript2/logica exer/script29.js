a907b887346ac981ba3bb66b54d0794bef9e00d8
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-16 18:10:22 -0300

    tabuada

---
+++ javaScript2/logica exer/script29.js
@@ -0,0 +1,11 @@
+function tabelaMatematica() {
+    return {
+        tabuada(n){
+            for (let i = 1; i <= 10; i++) {
+                console.log(n + 'x' + i + '=' + ' ' + n*i)
+            }
+        }
+    }
+}
+const tabela = tabelaMatematica()
+tabela.tabuada(2)
