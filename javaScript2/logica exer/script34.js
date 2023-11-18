13e390389d2957e924b14365eccfde6b99213095
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-18 16:55:57 -0300

    .

---
+++ javaScript2/logica exer/script34.js
@@ -0,0 +1,12 @@
+function pessoas(nome, idade) {
+    return {
+        infoPessoas() {
+            const informacoes = 
+        },
+        pessoaIdosa() {
+            const idade = this.infoPessoas.
+        }
+    }
+}
+const P1 = pessoas('joao', 80)
+P1.infoPessoas()
