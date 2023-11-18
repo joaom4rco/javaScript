11b4a962456a51c5200f78ec9ea996eda377ea78
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-18 15:44:09 -0300

    .

---
+++ javaScript2/logica exer/script32.js
@@ -0,0 +1,19 @@
+function notasSemestre(n1,n2,n3,n4) {
+    return {
+        aluno() {
+            this.mediaAluno()
+        },
+        notas() {
+            const notas = [n1,n2,n3,n4]
+            return notas
+        },
+        mediaAluno() {
+            const media = this.notas().reduce((c ,v) => {
+                return c + v 
+            }, 0) / this.notas.length()
+            console.log(media)
+        }
+    }
+}
+const aluno1 = notasSemestre(10,5,10,5) 
+aluno1.aluno()
