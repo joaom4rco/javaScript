d5b4e20f3e8601269acb9f7e03ce81d85a2edd5a
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-18 18:49:47 -0300

    .

--- javaScript2/logica exer/DOM15/script/pontuacao.js
+++ javaScript2/logica exer/DOM15/script/pontuacao.js
@@ -1,19 +1,39 @@
 const primeiroColocado = document.querySelector('#primeiro-colocado')
+const segundoColocado = document.querySelector('#segundo-colocado')
 const pontos = document.querySelector('#pontos')
 const partidas = document.querySelector('#partidas')
 const vitorias = document.querySelector('#vitorias')
+const vitorias2 = document.querySelector('#vitorias2')
 const empates = document.querySelector('#empates')
 const derrotas = document.querySelector('#derrotas')
 const golsFeitos = document.querySelector('#gols-pro')
 const saldoDeGols = document.querySelector('#saldo-de-gols')
 const ultimasPartidas = document.querySelector('#ultimas-partidas')
-const inputTime1 = document.querySelector('#itime')
-const inputTime2 = document.querySelector('#itime2')
 const button = document.querySelector('#button')
+
+let contagem = 0
+let vitoria = 0
 function brasileirao() {
     return {
+        time1: document.querySelector('#itime'),
+        time2: document.querySelector('#itime2'),
+        botao: button,
         partidas() {
-            
+            this.botao.addEventListener('click', e => {
+            contagem++
+            partidas.innerHTML = contagem
+            })
+        },
+        vitoria () {
+            this.botao.addEventListener('click', e => {
+                if(Number(this.time1.value) > Number(this.time2.value)) {
+                    vitoria++
+                    vitorias2.innerHTML = vitoria
+                }else if (Number(this.time1.value) > Number(this.time2.value))
+            })
         }
     }
 }
+const p = brasileirao()
+p.partidas()
+p.vitoria()
