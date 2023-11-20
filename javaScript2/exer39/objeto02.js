7799b6c004527385080f50d3be79a5528a5c8244
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-20 17:22:41 -0300

    .

--- javaScript2/exer39/objeto02.js
+++ javaScript2/exer39/objeto02.js
@@ -2,7 +2,13 @@ const carro = {
     marca: 'ford',
     ano:'2020'
 }
-console.log(carro)
+Object.defineProperties(carro, {
+    marca: {
+        writable:false
+    }
+})
+Object.freeze(carro)
+carro.ano = 2019
 console.log(Object.getOwnPropertyDescriptor(carro, 'marca'))
 console.log(Object.entries(carro))
 console.log(Object.values(carro))
