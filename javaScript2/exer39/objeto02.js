ef203f4d3e40ef165564ba12e5762066975f36ec
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-20 17:19:36 -0300

    .

---
+++ javaScript2/exer39/objeto02.js
@@ -0,0 +1,8 @@
+const carro = {
+    marca: 'ford',
+    ano:'2020'
+}
+console.log(carro)
+console.log(Object.getOwnPropertyDescriptor(carro, 'marca'))
+console.log(Object.entries(carro))
+console.log(Object.values(carro))
