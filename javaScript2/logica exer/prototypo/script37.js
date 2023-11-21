6dfbc1fd318072045bb500de54b5f68060e82c6b
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-21 18:09:16 -0300

    .

--- javaScript2/logica exer/prototypo/script37.js
+++ javaScript2/logica exer/prototypo/script37.js
@@ -25,6 +25,18 @@ Camiseta.prototype.aumento = function(quantia) {
 Camiseta.prototype.desconto = function(quantia) {
     this.valor = this.valor - (this.valor * (quantia /100))
 }
-const camisa = new Camiseta('camiseta', 50, 'vermelho');
+
+function Caneca (nome, valor, cor) {
+    Produto.call(this, nome, valor)
+    this.cor = cor
+}
+Caneca.prototype = Object.create(Camiseta.prototype )
+Caneca.prototype.constructor = Caneca
+
+const caneca = new Caneca('caneca', 40, 'preta')
+caneca.desconto(50)
+console.log(caneca)
+
+const camisa = new Camiseta('blusa de frio', 100, 'preta');
 camisa.aumento(50);
 console.log(camisa);
