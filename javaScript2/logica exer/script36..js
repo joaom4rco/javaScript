8b6a4ee9912a39bb7a443d42ab94e215a0e1ab7b
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-21 17:08:34 -0300

    .

--- javaScript2/logica exer/script36..js
+++ javaScript2/logica exer/script36..js
@@ -13,6 +13,23 @@ Produto.prototype.aumento = function (percentual) {
 const p1 = new Produto('camiseta', 100, 80)
 const p2 = new Produto('ps5', 3000, 100)
 
-p2.aumento(67)
-p2.promocao(40)
-console.log(p2)
+// p2.aumento(67)
+// p2.promocao(40)
+// console.log(p2)
+
+const p3 = Object.create(Produto.prototype, {
+    nome: {
+        value: 'iphone 13',
+        writable:true,
+        configurable:true,
+        enumerable:true
+    },
+    preco: {
+        value: 3500 ,
+        writable:true,
+        configurable:true,
+        enumerable:true
+    }
+})
+p3.promocao(5)
+console.log(p3)
