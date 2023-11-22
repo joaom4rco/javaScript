048d8109baa7863bc45700efa2525f31121dc360
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-22 18:45:47 -0300

    .

--- javaScript2/logica exer/DOM18/script.js
+++ javaScript2/logica exer/DOM18/script.js
@@ -14,6 +14,16 @@ function anoNascimentoUsuario() {
             const conta = Number(this.data()) - Number(this.idade.value) 
             console.log(conta)
             this.idade.value = ''
+            return conta
+        })
+    },
+    teste() {
+        this.button.addEventListener('click', e=> {
+            if(this.masculino.checked === true){
+                console.log('sou homem')
+            } else if (this.feminino.checked === true){
+                console.log('sou mulher')
+            }
         })
     }
   }  
@@ -21,3 +31,4 @@ function anoNascimentoUsuario() {
 
 const nascimento = anoNascimentoUsuario()
 nascimento.calculoIdade()
+nascimento.teste()
