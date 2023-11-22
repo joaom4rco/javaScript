24bf596c379ec07afb885c130b3772e4f696d977
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-22 19:14:11 -0300

    .

--- javaScript2/logica exer/DOM18/script.js
+++ javaScript2/logica exer/DOM18/script.js
@@ -4,6 +4,8 @@ function anoNascimentoUsuario() {
     button:document.querySelector('#button'),
     masculino: document.querySelector('#sexoMas'),
     feminino: document.querySelector('#sexoFe'),
+    imagem: document.querySelector('#imagem img'),
+    anoNascimento: null,
     data() {
     const dataAtual = new Date().getFullYear()
     return dataAtual
@@ -14,21 +16,35 @@ function anoNascimentoUsuario() {
             const conta = Number(this.data()) - Number(this.idade.value) 
             console.log(conta)
             this.idade.value = ''
-            return conta
+            this.anoNascimento = conta
+            this.imgHomem()
+            this.imgMulher()
         })
     },
-    teste() {
-        this.button.addEventListener('click', e=> {
-            if(this.masculino.checked === true){
-                console.log('sou homem')
-            } else if (this.feminino.checked === true){
-                console.log('sou mulher')
-            }
-        })
-    }
+    imgHomem() {
+        if(this.anoNascimento >= 60 && this.masculino.checked === true && this.feminino.checked === false ) {
+            console.log('ola')
+        }else if(this.anoNascimento >= 18 && this.anoNascimento < 60 && this.masculino.checked === true && this.feminino.checked === false) {
+            console.log('sou adulto')
+        }else if(this.anoNascimento < 18 && this.anoNascimento >= 11 && this.masculino.checked === true && this.feminino.checked === false) {
+            console.log('sou adolesçente')
+        }else if (this.anoNascimento < 11 && this.anoNascimento >= 1 && this.masculino.checked === true && this.feminino.checked === false) {
+            console.log('sou crinça')
+        }
+    },
+    imgMulher() {
+        if(this.anoNascimento >= 60 && this.feminino.checked === true && this.masculino.checked === false ) {
+            console.log('ola e m')
+        }else if(this.anoNascimento >= 18 && this.anoNascimento < 60 && this.feminino.checked === true && this.masculino.checked === false) {
+            console.log('sou adulta ')
+        }else if(this.anoNascimento < 18 && this.anoNascimento >= 11 && this.feminino.checked === true && this.masculino.checked === false) {
+            console.log('sou adolesçente e m')
+        }else if(this.anoNascimento < 11 && this.anoNascimento >= 2 && this.feminino.checked === true && this.masculino.checked === false) {
+            console.log('sou crinça e m')
+        }
+    },
   }  
 }
 
 const nascimento = anoNascimentoUsuario()
 nascimento.calculoIdade()
-nascimento.teste()
