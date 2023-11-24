c88c39d57fe49507f7eb35b9e6a042cd1fab8ff7
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-24 17:23:16 -0300

    .

--- javaScript2/logica exer/accript38.js
+++ javaScript2/logica exer/accript38.js
@@ -3,9 +3,8 @@
 function produto(nome, valor) {
     return {
         get cartao() {
-            let nomeDoProduto = nome
             const conta = valor + 20
-            console.log('nomeDoProduto:' + nomeDoProduto + ' ' +  'o seu valor pelo cartão ficou de:' + conta + 'R$')
+            console.log('nomeDoProduto:' + nome + ' ' +  'o seu valor pelo cartão ficou de:' + conta + 'R$')
         },
         get pix() {
             let nomeDoProduto = nome
