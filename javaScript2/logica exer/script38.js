9a27e02a3d9a41cd1107603665e03343315da0b0
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-28 15:26:33 -0300

    .

--- javaScript2/logica exer/script38.js
+++ javaScript2/logica exer/script38.js
@@ -1,17 +1,20 @@
 function Professor(nome, disciplina,  salario) {
     this.nome =  nome
     this.disciplina = disciplina
-    this.salario = salario
     let salarioPrivado = salario
-    Object.defineProperty(this, 'salario'), {
-        value:salario, 
+    Object.defineProperty(this, 'salario', {
         enumerable: true, 
         configurable: true,
 
         get: () => {
-            
+            return salarioPrivado;
+        },
+        set: valor => {
+            if(typeof salarioPrivado !== 'number')  console.log('o erro está no salario')
+            if
+            salarioPrivado = valor
         }
-    }
+    })
 }
 const p1 = new Professor('joao marco', 'matematica', 1500)
-console.log(p1)
+console.log(p1.salario)
