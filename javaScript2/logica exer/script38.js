47bec9ffb2ad62b3692301f85cee03331fc93c87
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-28 15:15:32 -0300

    .

--- javaScript2/logica exer/script38.js
+++ javaScript2/logica exer/script38.js
@@ -2,5 +2,16 @@ function Professor(nome, disciplina,  salario) {
     this.nome =  nome
     this.disciplina = disciplina
     this.salario = salario
+    let salarioPrivado = salario
+    Object.defineProperty(this, 'salario'), {
+        value:salario, 
+        enumerable: true, 
+        configurable: true,
+
+        get: () => {
+            
+        }
+    }
 }
 const p1 = new Professor('joao marco', 'matematica', 1500)
+console.log(p1)
