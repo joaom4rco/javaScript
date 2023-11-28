78e7d4f32c4b95328243fac044d854d54ec50b58
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-28 15:46:58 -0300

    .

--- javaScript2/logica exer/script38.js
+++ javaScript2/logica exer/script38.js
@@ -5,15 +5,20 @@ function Professor(nome, disciplina,  salario) {
 }
 Professor.prototype.descontoInss = function () {
     if(this.salario <= 1320) {
-        this.salario = this.salario - (this.salario * (9 / 100))
+        this.salario = this.salario - (this.salario * (7.5 / 100))
     }
     if(this.salario >= 1321 && this.salario <= 2571) {
-        this.salario = this.salario - (this.salario * (12/ 100))
+        this.salario = this.salario - (this.salario * (9/ 100))
     }
-    if(this.salario >= 2572 && this.salario <=) {
-        this.salario = this.salario - (this.salario * (percentual / 100))
+    if(this.salario >= 2572 && this.salario <= 3856) {
+        this.salario = this.salario - (this.salario * (12 / 100))
+    }
+    if(this.salario >= 3857) {
+        this.salario = this.salario - (this.salario * (14 / 100))
     }
 }
 
 
 const p1 = new Professor('joao marco', 'matematica', 1500)
+p1.descontoInss()
+console.log(p1)
