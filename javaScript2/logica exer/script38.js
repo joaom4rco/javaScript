b9f4baa408e32a3b55af0bb59cc7c16329b187ec
joaom4rco <140863268+joaom4rco@users.noreply.github.com>
2023-11-28 15:57:16 -0300

    .

--- javaScript2/logica exer/script38.js
+++ javaScript2/logica exer/script38.js
@@ -1,24 +1,22 @@
 function Professor(nome, disciplina,  salario) {
     this.nome =  nome
     this.disciplina = disciplina
-    this.salario = salario
+    this.salarioBruto = salario
 }
 Professor.prototype.descontoInss = function () {
-    if(this.salario <= 1320) {
-        this.salario = this.salario - (this.salario * (7.5 / 100))
+    let salarioLiquido = this.salarioBruto;
+    if(this.salarioBruto <= 1320) {
+        salarioLiquido = this.salarioBruto - (this.salarioBruto * (7.5 / 100));
+    } else if(this.salarioBruto >= 1321 && this.salarioBruto <= 2571) {
+        salarioLiquido = this.salarioBruto - (this.salarioBruto * (9 / 100));
+    } else if(this.salarioBruto >= 2572 && this.salarioBruto <= 3856) {
+        salarioLiquido = this.salarioBruto - (this.salarioBruto * (12 / 100));
+    } else if(this.salarioBruto >= 3857) {
+        salarioLiquido = this.salarioBruto - (this.salarioBruto * (14 / 100));
     }
-    if(this.salario >= 1321 && this.salario <= 2571) {
-        this.salario = this.salario - (this.salario * (9/ 100))
-    }
-    if(this.salario >= 2572 && this.salario <= 3856) {
-        this.salario = this.salario - (this.salario * (12 / 100))
-    }
-    if(this.salario >= 3857) {
-        this.salario = this.salario - (this.salario * (14 / 100))
-    }
+    return salarioLiquido;
 }
 
 
-const p1 = new Professor('joao marco', 'matematica', 1500)
-p1.descontoInss()
-console.log(p1)
+const p1 = new Professor('joao marco', 'ingles', 2000)
+console.log(p1.nome,'professor de:',  p1.disciplina, 'o seu salario é de:',p1.salarioBruto, 'com o desconto do inss ele fica de:',p1.descontoInss())
